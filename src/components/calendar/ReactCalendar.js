137422ec6922e1490617a218a6606a25af80d689
gvsax3322 <cosbe3822@gmail.com>
2024-03-24 02:07:25 +0900

    일정추가 연습중

--- src/components/calendar/ReactCalendar.js
+++ src/components/calendar/ReactCalendar.js
@@ -4,30 +4,60 @@ import {
   StyledCalendarWrapper,
 } from "../../styles/calendarstyle/calendarstyle";
 import moment from "moment";
+import { TodoWrap } from "../../styles/todostyles/todostyle";
 
 const ReactCalendar = () => {
   const today = new Date();
   const [date, setDate] = useState(today);
+  const [schedule, setSchedule] = useState({});
 
   const handleDateChange = newDate => {
     setDate(newDate);
   };
 
+  const handleDateClick = date => {
+    const newSchedule = prompt("일정을 입력하세요:");
+    if (newSchedule) {
+      const dateString = moment(date).format("YYYY-MM-DD");
+      setSchedule(prevSchedule => ({
+        ...prevSchedule,
+        [dateString]: [...(prevSchedule[dateString] || []), newSchedule],
+      }));
+    }
+  };
+
+  const getScheduleForDate = date => {
+    const dateString = moment(date).format("YYYY-MM-DD");
+    return schedule[dateString] || [];
+  };
+
   return (
-    <StyledCalendarWrapper>
-      <StyledCalendar
-        value={date}
-        onChange={handleDateChange}
-        formatDay={(locale, date) => moment(date).format("D")}
-        formatYear={(locale, date) => moment(date).format("YYYY")}
-        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
-        calendarType="gregory"
-        showNeighboringMonth={false}
-        next2Label={null}
-        prev2Label={null}
-        minDetail="year"
-      />
-    </StyledCalendarWrapper>
+    <TodoWrap>
+      <StyledCalendarWrapper>
+        <StyledCalendar
+          value={date}
+          onChange={handleDateChange}
+          onClickDay={handleDateClick}
+          formatDay={(locale, date) => moment(date).format("D")}
+          formatYear={(locale, date) => moment(date).format("YYYY")}
+          formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+          calendarType="gregory"
+          showNeighboringMonth={false}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="year"
+          tileContent={({ date }) => (
+            <div>
+              {getScheduleForDate(date).map((schedule, index) => (
+                <div style={{ marginBottom: 5 }} key={index}>
+                  {schedule}
+                </div>
+              ))}
+            </div>
+          )}
+        />
+      </StyledCalendarWrapper>
+    </TodoWrap>
   );
 };
 
--- src/styles/calendarstyle/calendarstyle.js
+++ src/styles/calendarstyle/calendarstyle.js
@@ -1,11 +1,109 @@
 import styled from "@emotion/styled";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+
 export const StyledCalendarWrapper = styled.div`
   width: 100%;
+  height: 750px;
   display: flex;
   justify-content: center;
   position: relative;
+  color: black;
+  .react-calendar {
+    width: 100%;
+    border: none;
+    border-radius: 0.5rem;
+    box-shadow: 4px 2px 10px 0px rgba(0, 0, 0, 0.13);
+    padding: 3% 5%;
+    background-color: white;
+  }
+
+  /* 네비게이션 가운데 정렬 */
+  .react-calendar__navigation {
+    justify-content: center;
+  }
+
+  /* 네비게이션 폰트 설정 */
+  .react-calendar__navigation button {
+    font-weight: 800;
+    font-size: 2rem;
+  }
+
+  /* 네비게이션 버튼 컬러 */
+  .react-calendar__navigation button:focus {
+    background-color: white;
+  }
+
+  /* 네비게이션 비활성화 됐을때 스타일 */
+  .react-calendar__navigation button:disabled {
+    background-color: white;
+  }
+
+  /* 년/월 상단 네비게이션 칸 크기 줄이기 */
+  .react-calendar__navigation__label {
+    flex-grow: 0 !important;
+  }
+
+  .react-calendar__month-view__weekdays abbr {
+    text-decoration: none;
+    font-weight: 800;
+  }
+
+  .react-calendar__month-view__weekdays__weekday {
+    font-size: 1.5rem;
+  }
+
+  /* 오늘 날짜 폰트 컬러 */
+  .react-calendar__tile--now {
+    background: skyblue;
+    abbr {
+      color: blue;
+    }
+  }
+
+  /* 네비게이션 월 스타일 적용 */
+  .react-calendar__year-view__months__month {
+    border-radius: 0.8rem;
+    padding: 0;
+  }
+
+  /* 네비게이션 현재 월 스타일 적용 */
+  .react-calendar__tile--hasActive {
+    background-color: skyblue;
+    abbr {
+      color: white;
+    }
+  }
+
+  /* 일 날짜 간격 */
+  .react-calendar__tile {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 5px;
+    align-items: center;
+    height: 100px;
+    font-size: 1.5rem;
+  }
+
+  /* 네비게이션 월 스타일 적용 */
+  .react-calendar__year-view__months__month {
+    flex: 0 0 calc(33.3333% - 10px) !important;
+    margin-inline-start: 5px !important;
+    margin-inline-end: 5px !important;
+    margin-block-end: 10px;
+    padding: 20px 6.6667px;
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
+
+  /* 선택한 날짜 스타일 적용 */
+  .react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus,
+  .react-calendar__tile--active {
+    background-color: rgb(135, 248, 248);
+    border-radius: 0.3rem;
+  }
 `;
 
 export const StyledCalendar = styled(Calendar)``;
